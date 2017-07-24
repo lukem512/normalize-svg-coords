f6bc6ed0d090914af90da6477212d9ca72f24e55
lukem512 <hi@lukemitchell.co>
2017-07-24 15:35:40 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const assert = require('assert')
-const isEqual = require('lodash.isEqual');
+const isEqual = require('lodash.isequal');
 const normalize = require('../index')
 
 describe('Normalize SVG path', function() {
